11c00ae073731865ea7f734eccee0ba25359aaba
kraghavendra <kraghavendra@matterport.com>
2022-03-03 00:00:29 -0600

    [Task-9874]: Intial Setup

---
+++ services/s3_bucket/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type        = string
+  description = "region name to deploy in e.g. us-east-1"
+}
+
+variable "aws_account" {
+  type        = string
+  description = "name of the aws account e.g. matterport-exp"
+}
