4fbb3b4c3ce10246fb31b84769898dd0cd1016a7
Ubuntu <ubuntu@ip-172-31-22-22.us-east-2.compute.internal>
2022-05-30 00:47:51 +0000

    [Fix]: Services and Modules

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,12 +1,7 @@
-resource "aws_s3_bucket" "b" {
-  bucket = $var.s3_bucket_name
-
-  tags = {
-    Name        = $var.tagname
-  }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.s3_bucket_name
   acl    = "private"
+  tags = {
+    Name = var.tagname
+  }
 }
--- services/s3_bucket/variables.tf
+++ services/s3_bucket/variables.tf
@@ -3,10 +3,10 @@ variable "aws_region" {
   description = "region name to deploy in e.g. us-east-1"
 }
 
-variable "aws_account" {
-  type        = string
-  description = "name of the aws account"
-}
+#variable "aws_account" {
+#  type        = string
+#  description = "name of the aws account"
+#}
 
 variable "s3_bucket_name" {
   type        = string
